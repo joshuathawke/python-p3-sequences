e5d7dcdb51e38a9efdce5b454dd401b9d0a3119a
joshuathawke <joshuathawke@gmail.com>
2023-04-03 16:57:44 -0400

    changes

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fib_sequence = []
+    if length == 0:
+        print(fib_sequence)
+        return
+    fib_sequence.append(0)
+    if length == 1:
+        print(fib_sequence)
+        return
+    fib_sequence.append(1)
+    if length == 2:
+        print(fib_sequence)
+        return
+    for i in range(2, length):
+        fib_sequence.append(fib_sequence[i-1] + fib_sequence[i-2])
+    print(fib_sequence)
